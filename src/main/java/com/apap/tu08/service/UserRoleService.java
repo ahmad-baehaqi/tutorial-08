cc1287a6384a170f8e95fac0ed2a0416dbc031d0
ahmad-baehaqi <ahbaehaqi03@gmail.com>
2020-04-14 22:43:16 +0700

    Push tutorial-08  ke repo Ahmad Baehaqi

---
+++ src/main/java/com/apap/tu08/service/UserRoleService.java
@@ -0,0 +1,9 @@
+package com.apap.tu08.service;
+
+import com.apap.tu08.model.UserRoleModel;
+
+public interface UserRoleService {
+    UserRoleModel addUser(UserRoleModel user);
+
+    public String encrypt(String password);
+}
